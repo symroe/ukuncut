17b54f9b5ebb884ae7af88695aced53da56165b9
Sym Roe <sym.roe@talusdesign.co.uk>
2010-12-15 17:56:24 +0000

    hacky way to get results from postcode if geolocate doesn't work

--- web/media/javascript/main.js
+++ web/media/javascript/main.js
@@ -10,5 +10,31 @@ function post_locate(geolocation_data) {
 }
 
 $(document).ready(function(){
-        $.geolocator.geolocate({ callback: post_locate});
+        $.geolocator.geolocate({ callback: post_locate});        
+        
+        $('#postcode_search').bind('click', function() {
+            postcode = $('#postcode').attr('value')
+            
+            if (postcode != '') {
+                url = 'http://mapit.mysociety.org/postcode/'+postcode+'?callback=?'
+                $('#loading').show();
+                $.getJSON(url, function(data) {
+                    if (data.error) {
+                        $('#loading').hide();
+                        // $('<span class="error">'+data.error+'</span>').insertAfter('#postcode');
+                        $('#error').html(data.error)
+                    } else {
+                        url = '/get_results/'+data.wgs84_lat+'/'+data.wgs84_lon;
+
+                        $('#unsupported').hide();
+
+                        $('#results').load(url, function(){$('#loading').hide();});
+                        $('#results').show();
+                    }
+                
+                });
+            }
+            return false;
+        })
+        
 });
